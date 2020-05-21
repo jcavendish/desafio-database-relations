d32b3e3aa329ad81bcb6888c61d96ff7ce52aed1
Joao Paulo Cavendish <joaopaulocavendish@gmail.com>
2020-05-21 16:25:57 -0300

    create costumers

--- src/modules/customers/services/CreateCustomerService.ts
+++ src/modules/customers/services/CreateCustomerService.ts
@@ -12,10 +12,21 @@ interface IRequest {
 
 @injectable()
 class CreateCustomerService {
-  constructor(private customersRepository: ICustomersRepository) {}
+  constructor(
+    @inject('CustomersRepository')
+    private customersRepository: ICustomersRepository,
+  ) {}
 
   public async execute({ name, email }: IRequest): Promise<Customer> {
-    // TODO
+    const conflictingCustomer = await this.customersRepository.findByEmail(
+      email,
+    );
+
+    if (conflictingCustomer) {
+      throw new AppError('This e-mail is already in use');
+    }
+
+    return this.customersRepository.create({ name, email });
   }
 }
 
