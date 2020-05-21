ce3a161baa6ada62b2d0b8d77373c50d897738b1
Joao Paulo Cavendish <joaopaulocavendish@gmail.com>
2020-05-21 17:43:35 -0300

    create products

--- src/modules/products/infra/typeorm/repositories/ProductsRepository.ts
+++ src/modules/products/infra/typeorm/repositories/ProductsRepository.ts
@@ -1,4 +1,4 @@
-import { getRepository, Repository, In } from 'typeorm';
+import { getRepository, Repository } from 'typeorm';
 
 import IProductsRepository from '@modules/products/repositories/IProductsRepository';
 import ICreateProductDTO from '@modules/products/dtos/ICreateProductDTO';
@@ -7,6 +7,10 @@ import Product from '../entities/Product';
 
 interface IFindProducts {
   id: string;
+}
+
+interface IProductLookup {
+  [key: string]: number;
 }
 
 class ProductsRepository implements IProductsRepository {
@@ -21,21 +25,46 @@ class ProductsRepository implements IProductsRepository {
     price,
     quantity,
   }: ICreateProductDTO): Promise<Product> {
-    // TODO
+    const product = this.ormRepository.create({ name, price, quantity });
+
+    return this.ormRepository.save(product);
   }
 
   public async findByName(name: string): Promise<Product | undefined> {
-    // TODO
+    return this.ormRepository.findOne({
+      where: {
+        name,
+      },
+    });
   }
 
   public async findAllById(products: IFindProducts[]): Promise<Product[]> {
-    // TODO
+    const productIds = products.map(product => product.id);
+
+    return this.ormRepository.findByIds(productIds);
   }
 
   public async updateQuantity(
     products: IUpdateProductsQuantityDTO[],
   ): Promise<Product[]> {
-    // TODO
+    const quantityLookup = {} as IProductLookup;
+    products.forEach(product => {
+      quantityLookup[product.id] = product.quantity;
+    });
+
+    const productIds = Object.keys(quantityLookup);
+    const foundProducts = await this.ormRepository.findByIds(productIds);
+
+    const updatedProducts = foundProducts.map(product => {
+      const quantity = quantityLookup[product.id];
+
+      return {
+        ...product,
+        quantity,
+      };
+    });
+
+    return this.ormRepository.save(updatedProducts);
   }
 }
 
--- src/modules/products/services/CreateProductService.ts
+++ src/modules/products/services/CreateProductService.ts
@@ -13,10 +13,19 @@ interface IRequest {
 
 @injectable()
 class CreateProductService {
-  constructor(private productsRepository: IProductsRepository) {}
+  constructor(
+    @inject('ProductsRepository')
+    private productsRepository: IProductsRepository,
+  ) {}
 
   public async execute({ name, price, quantity }: IRequest): Promise<Product> {
-    // TODO
+    const conflictingProduct = await this.productsRepository.findByName(name);
+
+    if (conflictingProduct) {
+      throw new AppError('This product already exists');
+    }
+
+    return this.productsRepository.create({ name, price, quantity });
   }
 }
 
